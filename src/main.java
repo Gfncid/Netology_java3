5fe1ab0f9f44088dfcaf3549e16077537047ab8d
Aleksandr Zarubin <aljkeo.O@gmail.com>
2020-06-09 13:22:32 +0300

    files added

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        int balance = 2000000000;
+        int order = 500000000;
+        int total = balance + order;
+
+        System.out.println(total);
+
+    }
+}
